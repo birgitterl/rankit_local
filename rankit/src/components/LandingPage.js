58ad0d04d8a959797de73a9dffebaa3dab986c85
Gerstlacher <j.gerstlacher@gmx.de>
2020-01-09 10:40:35 +0100

    changed LandingPage

--- rankit/src/components/LandingPage.js
+++ rankit/src/components/LandingPage.js
@@ -1,54 +1,43 @@
+//TODO: Admin-Button, insert name field, submit button
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { Container } from 'reactstrap';
-import UserModal from './UserModal';
+import { Container, Button, Form, Input, FormGroup, FormText, Label, Col } from 'reactstrap';
 
 class LandingPage extends Component {
 	constructor(props) {
 		super(props);
+		this.state = { value: '' };
 
-		this.state = { username: '' };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	componentDidMount() {
-		this.setState({
-			users: ['test user'],
-			username: 'test user'
-		});
+	handleChange(event) {
+		this.setState({ value: event.target.value });
 	}
 
-	onChangeUsername(e) {
-		this.setState({
-			username: e.target.value
-		});
+	handleSubmit(event) {
+		alert('A name was submitted: ' + this.state.value);
+		event.preventDefault();
 	}
 
 	render() {
 		return (
 			<div>
 				<Container>
-					<h1 className="p-5 text-center">Welcome to Rankit</h1>
-					<p className="p-5 text-center">Start the game by adding your name</p>
-					<form onSubmit={this.onSubmit}>
-						<div className="form-group">
-							<label>Username: </label>
-							<input
-								type="text"
-								required
-								value={this.state.username}
-								onChange={this.onChangeUsername}
-								className="form-control"
-							/>
-						</div>
-						<div className="form-group">
-							<input
-								type="submit"
-								value="Create User"
-								className="btn btn-primary"
-							/>
-						</div>
-					</form>
+					<h1>LandingPage</h1>
+					<p></p>
+					<p>Welcome to Rankit!</p>
+					<p>Join the game :-)</p>
+					<Form onSubmit={this.handleSubmit}>
+						<FormGroup row>
+							<Label for="userName" sm={1}>Name:</Label>
+							<Col sm={10}>
+								<Input type="text" value={this.state.value}
+									onChange={this.handleChange} />
+							</Col>
+						</FormGroup>
+						<Button color="primary">Submit</Button>
+					</Form>
 				</Container>
 			</div>
 		);
