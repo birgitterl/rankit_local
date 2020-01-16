d727984fd67709abae7acda349303b097261c2ef
birgitterl <schneiderbirgit@gmx.at>
2020-01-16 08:18:12 +0100

    updated Router

--- rankit/src/components/routes/Routes.js
+++ rankit/src/components/routes/Routes.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AdminHub from '../AdminHub';
-import Login from '../AdminLogin';
+import AdminLogin from '../AdminLogin';
 import PlayersView from '../PlayersView';
+import Container from 'reactstrap/es/Container';
 
 const Routes = () => {
 	return (
-		<section className="container">
+		<Container>
 			<Switch>
-				<Route exact path="/rankit" component={PlayersView} />
-				<Route exact path="/login" component={Login} />
+				<Route exact path="/playersView" component={PlayersView} />
+				<Route exact path="/adminLogin" component={AdminLogin} />
 				<Route exact path="/adminHub" component={AdminHub} />
 			</Switch>
-		</section>
+		</Container>
 	);
 };
 
