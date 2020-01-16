445a4381a9f0279cea188a74452eaebba42be501
birgitterl <schneiderbirgit@gmx.at>
2020-01-16 08:18:45 +0100

    created PlayersView and added styling (app.css)

--- rankit/src/components/PlayersView.js
+++ rankit/src/components/PlayersView.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { getCurrentUser, updateVote } from '../actions/userActions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from './Spinner';
 
-function PlayersView(props) {
-	return (
-		<div>
-			<h1>Hello there</h1>
-		</div>
+const PlayersView = ({ getCurrentUser, updateVote, auth: { user } }) => {
+	useEffect(() => {
+		getCurrentUser();
+	}, [getCurrentUser]);
+
+	return user === null ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<div className="player-inner">
+				<h1>Hello {user.name}</h1>
+				<br />
+				<img src={user.avatar} className="rounded-circle" alt="Your Avatar" />
+				<br />
+				<h5>Your current score is {user.points} points</h5>
+				<br />
+				<h5>Press the button to rank-up</h5>
+				<br />
+				<button
+					type="button"
+					className="btn btn-success btn-circle btn-xl"
+					onClick={() => updateVote()}
+				>
+					Vote
+				</button>
+			</div>
+		</Fragment>
 	);
-}
+};
 
-export default PlayersView;
+PlayersView.propTypes = {
+	getCurrentUser: PropTypes.func.isRequired,
+	updateVote: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+	auth: state.userReducer
+});
+
+export default connect(mapStateToProps, { getCurrentUser, updateVote })(
+	PlayersView
+);
