61b4e35b32f1892b9e6bec04c5d8804ff8a85de7
andy9341 <k11702415@students.jku.at>
2020-01-20 19:51:48 +0100

    added the location of all users to the map

--- rankit/src/components/AdminHub/LocationMap.js
+++ rankit/src/components/AdminHub/LocationMap.js
@@ -1,22 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { getUsers } from '../../actions/userActions';
+import L from 'leaflet';
 
-class LocationMap extends Component {
-	//{getUsers().map(user => <Marker id={user.name} position={[user.latitude, user.longitude]} />)}
+//TODO: icon not found
+const runner = L.icon({
+	iconUrl: 'runner.png',
+	iconSize: [32, 38]
+	//iconAnchor: [25, 16]
+});
 
-	render() {
-		return (
-			<div>
-				<Map center={[48.336405, 14.32049]} zoom={16}>
-					<TileLayer
-						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+const LocationMap = ({ getUsers, userState: { users, loading } }) => {
+	useEffect(() => {
+		getUsers();
+	}, [getUsers]);
+	users.map(user => console.log(user));
+
+	return (
+		<div>
+			<Map center={[48.336405, 14.32049]} zoom={16}>
+				<TileLayer
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				/>
+
+				{users.map(user => (
+
+					<Marker
+						key={user._id}
+						position={[user.location.latitude, user.location.longitude]}
+						icon={runner}
 					/>
-				</Map>
-			</div>
-		);
-	}
+
+				))}
+			</Map>
+		</div>
+	);
 }
 
-export default LocationMap;
+LocationMap.propTypes = {
+	getUsers: PropTypes.func.isRequired,
+	userState: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	userState: state.userReducer
+});
+
+export default connect(mapStateToProps, { getUsers })(LocationMap);
